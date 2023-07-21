baa2095622cda94765a34c4e405f6cec0877beb7
nguyenvubang <nguyenvubang@kng.vn>
2023-07-21 10:01:40 +0700

    add db url

--- src/main/java/org/example/utils/StringUtils.java
+++ src/main/java/org/example/utils/StringUtils.java
@@ -3,4 +3,7 @@ package org.example.utils;
 public class StringUtils {
     public static String TOKEN = "YOUR_TOKEN";
     public static String IMDB_API_KEY = "API_KEY";
+    public static String DB_URL = "DB_URL";
+    public static String USER_NAME = "USER_NAME";
+    public static String PASSWORD = "PASSWORD";
 }
